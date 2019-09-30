e7932214683b1fb0f542b21b669a2af37f571ba1
Cynthia <c_martiny@outlook.com>
2019-09-30 15:35:22 +0200

    récup données DB

---
+++ src/index.js
@@ -0,0 +1,29 @@
+var axios = require("axios");
+// Make a request for a user with a given ID
+axios
+  .get("https://character-database.becode.xyz/characters/")
+  .then(function(response) {
+    var array = response.data;
+    var div = "<div>";
+    array.forEach(function(element) {
+      div += "<div>";
+      div += '<img src="data:image;base64,' + element.image + '"/>';
+      div += "<ul>";
+      div += "<li>" + element.name + "</li>";
+      div += "<li>" + element.shortDescription + "</li>";
+      div += "</ul>";
+      div += "<button>View</button>";
+      div += "<button>Edit</button>";
+      div += "<button>Delete</button>";
+      div += "</div>";
+    });
+    div += "</div>";
+    document.getElementById("root").innerHTML = div;
+  })
+  ["catch"](function(error) {
+    // handle error
+    console.log(error);
+  })
+  ["finally"](function() {
+    // always executed
+  });
